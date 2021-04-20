692f1754260bccbdec879c6b9f520cf8c4f91556
marco.bertelli-at-170187898933 <marcobert37@gmail.com>
2021-04-20 14:37:59 +0200

    tabs manage orders

--- src/pages/ManageOrders.tsx
+++ src/pages/ManageOrders.tsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ManageOrderItems from "../components/manage-orders/ManageOrderItems";
 import Spinner from "../components/Spinner";
 import Tab from "../components/Tab";
 import { orderTabs } from "../helpers";
+import { useSelectTab } from "../hooks/useSelectTab";
 import { useOrdersContext } from "../state/orders-context";
 import { useSearchContext } from "../state/search-context";
-import { Order } from "../types";
+import { Order, OrderTab } from "../types";
+import { orderTabType } from "./Order";
 
 interface Props {}
 
 const ManageOrders: React.FC<Props> = () => {
   const {
-    ordersState: { orders, loading, error},
+    ordersState: { orders, loading, error },
   } = useOrdersContext();
-  const {searchedItems} = useSearchContext()
+  const { searchedItems } = useSearchContext();
+
+  const { activeTab } = useSelectTab<OrderTab>(orderTabType, "New");
+  const [ordersByTab, setOrdersByTab] = useState(
+    orders ? orders.filter((order) => order.shipmentStatus === "New") : null
+  );
+  //filtro ordini in base alla tab es new ecc
+  useEffect(() => {
+    if (!orders) {
+      setOrdersByTab(null);
+      return;
+    }
+
+    if (activeTab === "All") setOrdersByTab(orders);
+    else
+      setOrdersByTab(
+        orders.filter((order) => order.shipmentStatus === activeTab)
+      );
+  }, [activeTab, orders, setOrdersByTab]);
   if (loading) return <Spinner color="grey" height={50} width={50} />;
 
   if (error) return <h2 className="header-center">{error}</h2>;
@@ -26,8 +46,15 @@ const ManageOrders: React.FC<Props> = () => {
       <div className="orders-header">
         <h2 className="header header--orders">I tuoi Ordini</h2>
 
-        <div className="orders tab">
-        
+        <div className="orders-tabs">
+          {orderTabs.map((tab) => (
+            <Tab
+              key={tab}
+              label={tab}
+              activeTab={activeTab}
+              tabType={orderTabType}
+            />
+          ))}
         </div>
       </div>
 
@@ -55,16 +82,21 @@ const ManageOrders: React.FC<Props> = () => {
         </div>
 
         {/* ordini */}
-        {searchedItems ? <>
-        {searchedItems.length < 1 ? <h2 className="header--center">Nessun Ordine</h2>
-          : (searchedItems as Order[]).map((order) => (
+        {searchedItems ? (
+          <>
+            {searchedItems.length < 1 ? (
+              <h2 className="header--center">Nessun Ordine</h2>
+            ) : (
+              (searchedItems as Order[]).map((order) => (
+                <ManageOrderItems key={order.id} order={order} />
+              ))
+            )}
+          </>
+        ) : (
+          ordersByTab && ordersByTab.map((order) => (
             <ManageOrderItems key={order.id} order={order} />
           ))
-        }
-        
-         </> : orders.map((order) => (
-          <ManageOrderItems key={order.id} order={order} />
-        ))}
+        )}
       </div>
     </div>
   );
